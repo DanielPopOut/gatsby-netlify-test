f35b4d0e126e33568823701d9e3b08b7475fbbe9
DanielPopOut <stephane.tchangang@gmail.com>
2019-03-11 14:16:53 +0100

    DT test on branch stagging

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p>I modified this and this</p>
+    <p>I'm on the stagging branch yeah ! </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
